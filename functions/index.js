5c3029ef63a90afa759ada0fb92b090a86bcda54
Unknown <mathies-h@hotmail.com>
2019-03-14 16:57:21 +0100

    Added Time booking

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,30 @@
 const { dialogflow, Permission, Suggestions, BasicCard } = require("actions-on-google");
 const functions = require("firebase-functions");
 const app = dialogflow({ debug: true });
+const dateformat = require("dateformat");
 
 app.intent("Book", (conv, { Room_Number }) => {
-		conv.ask(
-            'you said ' + Room_Number +'. What period would you like to book?'
-		);
+    conv.data.room = Room_Number;
+    if(conv.data.date != null){
+        conv.ask(response + "Who would you like to book with?")
+    } else {
+        conv.ask("you said " + Room_Number + ". What period would you like to book?");
+    }
+});
+
+app.intent("BookTime", (conv, data) => {
+    conv.data.date = data.date;
+    conv.data.timeperiod = data.timeperiod;
+    let date =  new Date(data.date);
+    let start = new Date(data['time-period'].startTime)
+    let end = new Date(data['time-period'].endTime)
+    
+    let response = `you said ${dateformat(date, "dd/mm")} from ${dateformat(start, "HH:MM")} to ${dateformat(end, "HH:MM")}. `;
+    if(conv.data.room != null){
+        conv.ask(response + "Who would you like to book with?")
+    } else {
+        conv.ask(response + "Which room would you like to book?")
+    }
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
